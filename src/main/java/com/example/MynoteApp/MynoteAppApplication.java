892b876683e1c5cd779b8165fe6a82fc2ed14219
Croop-weed <harsh.arror@gmail.com>
2025-08-09 23:57:22 +0530

    Starting this project

---
+++ src/main/java/com/example/MynoteApp/MynoteAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.MynoteApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MynoteAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MynoteAppApplication.class, args);
+	}
+
+}
